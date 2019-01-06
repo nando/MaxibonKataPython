3c4ed8550f9aaff03ce8241d57eeace9d82d3192
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2019-01-06 12:31:55 +0100

    should start the day with 10 maxibons property

---
+++ maxibonkata/karumihqs.py
@@ -0,0 +1,15 @@
+"""maxibonkata.karumihqs
+
+This module contains the KarumiHQs class implementation.
+
+:copyright: 2019, Karumi & The Cocktail
+
+:license: Apache License. See LICENSE.txt file for further details.
+"""
+
+class KarumiHQs:
+    def __init__( self, office_name = "KarumiHQs" ):
+        self.maxibons_left = 10
+
+    def maxibonsLeft( self ):
+        return self.maxibons_left
---
+++ maxibonkata/test/karumihqs_test.py
@@ -0,0 +1,20 @@
+"""maxibonkata.test.karumihqstest
+
+This module contains property tests for maxibonkata.karumihqs KarumiHQs class.
+
+:copyright: 2019, Karumi & The Cocktail
+
+:license: Apache License. See LICENSE.txt file for further details.
+"""
+
+import pytest
+
+from hypothesis import given
+from hypothesis import strategies as st
+
+from karumihqs import KarumiHQs
+
+@given( office_name = st.just( "Madrid KarumiHQs" ))
+def test_should_start_the_day_with_10_maxibons( office_name ):
+    office = KarumiHQs( office_name )
+    assert office.maxibonsLeft(), 10
