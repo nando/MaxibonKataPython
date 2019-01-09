8ebeaf1ab031f4914284cdab4beeba572a0b83ad
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2019-01-09 11:47:07 +0100

    always more than 2 maxibons in the fridge property

--- maxibonkata/karumihqs.py
+++ maxibonkata/karumihqs.py
@@ -8,8 +8,18 @@ This module contains the KarumiHQs class implementation.
 """
 
 class KarumiHQs:
+    MIN_MAXIBONS = 2
+    MAX_MAXIBONS = 10
+
     def __init__( self, office_name = "KarumiHQs" ):
         self.maxibons_left = 10
 
     def maxibonsLeft( self ):
+        return self.maxibons_left
+
+    def openFridge( self, developer ):
+        self.maxibons_left = max(0, self.maxibons_left - developer.maxibonsToGrab())
+        if self.maxibons_left <= KarumiHQs.MIN_MAXIBONS:
+            self.maxibons_left = self.maxibons_left + KarumiHQs.MAX_MAXIBONS
+
         return self.maxibons_left
--- maxibonkata/test/karumihqs_test.py
+++ maxibonkata/test/karumihqs_test.py
@@ -13,8 +13,15 @@ from hypothesis import given
 from hypothesis import strategies as st
 
 from karumihqs import KarumiHQs
+from generators import developers
 
 @given( office_name = st.just( "Madrid KarumiHQs" ))
 def test_should_start_the_day_with_10_maxibons( office_name ):
     office = KarumiHQs( office_name )
     assert office.maxibonsLeft() == 10
+
+@given( developers )
+def test_should_always_has_more_than_two_maxibons_in_the_fridge( developer ):
+    office = KarumiHQs()
+    office.openFridge( developer )
+    assert office.maxibonsLeft() >= 2
