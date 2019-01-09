5f691be72e4c28a97c94c21c853cd8c1a1bf1800
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2019-01-09 12:27:10 +0100

    assign the developer name in construction property

--- maxibonkata/test/developer_test.py
+++ maxibonkata/test/developer_test.py
@@ -14,6 +14,11 @@ from hypothesis.strategies import integers, text
 
 from developer import Developer
 
+@given( text() )
+def test_assign_the_name_of_the_developer_in_construction( name ):
+    developer = Developer( name )
+    assert developer.name == name
+
 @given( text(), integers() )
 def test_should_always_grab_a_positive_number_of_maxibons( name, mtg ):
     developer = Developer( name, mtg )
