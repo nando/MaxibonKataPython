8446ae443996275ba07c4f956987c8ce5a1658a4
Fernando Garcia Samblas <fernando.garcia@the-cocktail.com>
2019-01-10 20:28:09 +0100

    assign # of maxibons to every developer property

--- maxibonkata/developer.py
+++ maxibonkata/developer.py
@@ -14,3 +14,18 @@ class Developer:
 
     def maxibonsToGrab( self ):
         return self.maxibons_to_grab
+
+    def pedro():
+        return { "name": "Pedro", "maxibons_to_grab": 3 }
+
+    def fran():
+        return { "name": "Fran", "maxibons_to_grab": 1 }
+
+    def davide():
+        return { "name": "Davide", "maxibons_to_grab": 0 }
+
+    def sergio():
+        return { "name": "Sergio", "maxibons_to_grab": 2 }
+
+    def jorge():
+        return { "name": "jorge", "maxibons_to_grab": 1 }
--- maxibonkata/test/developer_test.py
+++ maxibonkata/test/developer_test.py
@@ -10,7 +10,7 @@ This module contains property tests for maxibonkata.developer Developer class.
 import pytest
 
 from hypothesis import given
-from hypothesis.strategies import integers, text
+from hypothesis.strategies import integers, text, sampled_from
 
 from developer import Developer
 
@@ -23,3 +23,15 @@ def test_assign_the_name_of_the_developer_in_construction( name ):
 def test_should_always_grab_a_positive_number_of_maxibons( name, mtg ):
     developer = Developer( name, mtg )
     assert developer.maxibonsToGrab() >= 0
+
+@given( sampled_from([
+    ( Developer.pedro, 3 ),
+    ( Developer.fran, 1 ),
+    ( Developer.davide, 0 ),
+    ( Developer.sergio, 2 ),
+    ( Developer.jorge, 1 )
+]) )
+def test_assign_the_number_of_maxibons_specified_to_every_developer( tuple ):
+    developer = tuple[0]
+    maxibons_to_grab = tuple[1]
+    assert developer()["maxibons_to_grab"] == maxibons_to_grab
